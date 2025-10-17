7ad49eae1dab7e97050a5534b131e0c54aefcf77
Dave <david.tomusk@gmail.com>
2025-10-17 10:18:06 +0100

    Add second consumer with just one binding

---
+++ message-queue/direct_exchange/consumer_info.py
@@ -0,0 +1,26 @@
+import pika
+
+from models import severity
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+result = channel.queue_declare(queue='', exclusive=True)
+queue_name = result.method.queue
+
+# binds just the info severity, probably not very practical, but good for practice
+channel.queue_bind(exchange='foo',
+                    queue=queue_name,
+                    routing_key=severity.info.name)
+    
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
+
+def callback(ch, method, properties, body):
+    print(f" [x] {method.routing_key}:{body}")
+
+
+channel.basic_consume(
+    queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+channel.start_consuming()
